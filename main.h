bcb400d89f5aa83190e955a84fe2bf01bc425e27
deantosh <deantoshdaiddoh@gmail.com>
2023-07-17 03:55:18 -0700

    print_env and exit function prototypes added

--- main.h
+++ main.h
@@ -10,15 +10,34 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+/*Global veriable*/
+extern  char **environ;
+
 /*MACROS*/
 #define PROMPT "#cisfun$"
+
+/*command struct*/
+/**
+ * struct builtin_command - Creates a type for the command and
+ *				its corresponding function.
+ * @name: A pointer to the command name.
+ * @func: A function pointer to its corresponding pointer.
+ *
+ * Description: struct for builtin commands.
+ */ 
+typedef struct builtin_command{
+	char *name;
+	int (*func)(char **);
+} builtin_s;
 
 /*file: main.c*/
 int get_token(char *str, const char *delim);
 char **create_av(char *str, int tokens, const char *delim);
 
 /*file: execute_command.c*/
-void execute_command(char **argv);
+int execute_builtin_command(char **argv);
+int get_builtin_len(builtin_s cmd_list[]);
+int execute_external_command(char **argv);
 
 /*file: get_executable_path.c*/
 char *get_full_path(char *command);
@@ -26,5 +45,13 @@ char *create_path(char *token, char *command);
 
 /*file: memory.c*/
 void free_pp(char **pp);
+
+/*BUILTIN COMMANDS*/
+
+/*file: exit.c*/
+int exit_shell(char **argv);
+
+/*file: env.c*/
+int print_env(char **argv);
 
 #endif /*MAIN_H*/
