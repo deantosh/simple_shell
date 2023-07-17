eb680525ea0feef04aa6d3242d722f0616b85915
deantosh <deantoshdaiddoh@gmail.com>
2023-07-17 03:57:25 -0700

    builtin command function created

--- execute_command.c
+++ execute_command.c
@@ -6,12 +6,66 @@
 #include "main.h"
 
 /**
- * execute_command - Executes system commands.
+ * execute_builtin_command - Executes commands builtin
+ *				to the program.
+ * @argv: The lisst of arguments used by the command.
+ *
+ * Return: 0 (success) and 1 (fails).
+ */
+int execute_builtin_command(char **argv)
+{
+	/*declare variables*/
+	int index, builtin_len, status, result;
+	char *command = argv[0];
+	builtin_s cmd_list[] = {
+		{"exit", exit_shell},
+		{"env", print_env},
+		{NULL, NULL}
+	};
+
+	/*check if command provided exists*/
+	if (command == NULL)
+		return (1);
+
+	/*get len of the cmd_list*/
+	builtin_len = get_builtin_len(cmd_list);
+	for (index = 0; index < builtin_len; index++)
+	{
+		status = strcmp(cmd_list[index].name, command);
+		if (status == 0)
+		{
+			/*execute command*/
+			result = cmd_list[index].func(argv);
+			return (result);
+		}
+	}
+	return (1);
+}
+
+/**
+ * get_builtin_len - Gets the number of items in a list.
+ * @cmd_list: The array of commands.
+ *
+ * Return: The length of the list or 0 (none);
+ */
+int get_builtin_len(builtin_s cmd_list[])
+{
+	/*declare variable*/
+	int index = 0;
+
+	while (cmd_list[index].name != NULL)
+		index++;
+
+	return (index);
+}
+
+/**
+ * execute_external_command - Executes system commands.
  * @argv: The list of arguments used by the command.
  *
- * Return: 1 (success).
+ * Return: 0 (success) or 1 (fails).
  */
-void execute_command(char **argv)
+int execute_external_command(char **argv)
 {
 	char *command = NULL, *full_command = NULL;
 	int status;
@@ -30,19 +84,11 @@ void execute_command(char **argv)
 			if (pid == 0)
 			{
 				if (execve(full_command, argv, NULL) == -1)
-				{
-					perror(argv[0]);
-					exit(1);
-				};
+					return (1);
 			}
-			else if (pid > 0)
-			{
-				wait(&status);
-			}
-			else
-				perror("Error:");
-
+			wait(&status);
 			free(full_command); /*free memory*/
 		}
 	}
+	return (0);
 }
