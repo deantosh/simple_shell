39a3e4d7d9394c30cc075d4e977f50fd7f9b213d
luckys-lnz <lalucky22@gmail.com>
2023-07-25 08:44:11 +0000

    test this

--- change_directory.c
+++ change_directory.c
@@ -42,14 +42,14 @@ int change_directory(const char *path)
     return (0);
 }
 
-int main(int argc, char *argv)
-{
-    char *home_dir = getenv("HOME");
-    char *cur_work_dir = getcwd(NULL, 0);
+// int main(int argc, char *argv)
+// {
+//     char *home_dir = getenv("HOME");
+//     char *cur_work_dir = getcwd(NULL, 0);
 
-    /*if arg is not given, change directory $PATH to $HOME*/
-    if (argc ==1 || (argc ==2 && strcmp(argv[1], "~") == 0))
-    {
-        chdir(home_dir);
-    }
-}
+//     /*if arg is not given, change directory $PATH to $HOME*/
+//     if (argc ==1 || (argc ==2 && strcmp(argv[1], "~") == 0))
+//     {
+//         chdir(home_dir);
+//     }
+// }
