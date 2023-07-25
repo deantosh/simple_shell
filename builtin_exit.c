38c4042919c5a617e7680f081b72013a1cd50531
deantosh <deantoshdaiddoh@gmail.com>
2023-07-25 09:39:53 -0700

    exit_shell command added

---
+++ builtin_exit.c
@@ -0,0 +1,24 @@
+/*
+ * File: exit_shell.c
+ * Author: Deantosh Daiddoh & Lucky Archibong
+ */
+
+#include "main.h"
+
+/**
+ * exit_shell - Exits shell.
+ * @argv: A pointer to the list of arguments.
+ *
+ * Return: 2 (success) or exit_status (if provided).
+ */
+int exit_shell(char **argv)
+{
+	int exit_status;
+
+	if (argv[1]) /*check if exit status is provided*/
+	{
+		exit_status = atoi(argv[1]);
+		return (exit_status);
+	}
+	return (2); /*if exit status not provided*/
+}
