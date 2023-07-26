48348fe675b658164f7fbbbb3bf862621a3cfa4e
luckys-lnz <lalucky22@gmail.com>
2023-07-26 19:41:48 +0000

    cd file update

--- builtin_cd.c
+++ builtin_cd.c
@@ -6,7 +6,7 @@
 
 /**
  *get_pwd_env - Update the PWD environment
- *         to current working directory.
+ *        to current working directory.
  */
 
 void get_pwd_env(void)
@@ -38,42 +38,5 @@ int change_directory(const char *path)
     }
 
     get_pwd_env();
-    return (0);
-}
-
-/**
- *executor - Entry point of the program.
- *@argc: The number of command-line arguments.
- *@argv: An array of strings containing the command-line arguments.
- *
- *Return: On success, 0. On incorrect usage or error, 1.
- */
-
-int executor(int argc, char **argv)
-{
-    char *home_dir = getenv("HOME");
-
-    /*if arg is not given, change directory $PATH to $HOME*/
-    if (argc == 1 || (argc == 2 && strcmp(argv[1], "~") == 0))
-    {
-        change_directory(home_dir);
-    }
-    else if (argc == 2)
-    /*change the directory to the specified $PATH*/
-    {
-        if (change_directory(argv[1]) == -1)
-        {
-            fprintf(stderr, "bad command");
-            return (1);
-            /*return a non zero value to indicate error*/
-        }
-    }
-    else
-    {
-        fprintf(stderr, "usage: %s[path]\n", argv[0]);
-        return (1);
-        /*To indicate incorrect, i had to return non-negative value*/
-    }
-
     return (0);
 }
