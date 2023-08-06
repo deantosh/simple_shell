a8b4a44b4277bc8c67f78e8d7e8dcd11ac02dadf
deantosh <deantoshdaiddoh@gmail.com>
2023-08-05 23:55:13 -0700

    custome getline function added to main

--- main.c
+++ main.c
@@ -21,12 +21,10 @@ int main(void)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("%s ", PROMPT);
-<<<<<<< HEAD
-		n_bytes = getline(&str, &n, stdin);
-=======
+
 		/*read from stdin stream*/
 		n_bytes = custom_getline(&str, &n, stdin);
->>>>>>> main
+
 		if (n_bytes == -1)
 		{
 			free(str);
